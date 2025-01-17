646c294e2db1bdd215a4cedfe62537d932e65181
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2025-01-17 18:15:52 -0300

    debbug

--- ProjetoEvento/src/App.jsx
+++ ProjetoEvento/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Questoes from './pages/Questoes';
+import AuthPage from './pages/login';
+import TestConnection from './pages/TestConnection'
+
+
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/questions" element={<Questoes />} />
+        <Route path="quizz" element={<Questoes />} />
+        <Route path="/" element={<AuthPage />} />
+        <Route path='test' element={<TestConnection/>}/>
       </Routes>
     </div>
+    
   );
 }
 
