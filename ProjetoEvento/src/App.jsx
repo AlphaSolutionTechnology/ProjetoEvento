7432c28396819eeb927e1ac8b971bf4b56a3a135
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2025-01-17 19:04:26 -0300

    puts

--- ProjetoEvento/src/App.jsx
+++ ProjetoEvento/src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Questoes from './pages/Questoes';
-
-
-
+import CreateQuestoes from './pages/createquestoes';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="quizz" element={<Questoes />} />
+        <Route path="console" element={<CreateQuestoes />} />
       </Routes>
     </div>
-    
   );
 }
 
