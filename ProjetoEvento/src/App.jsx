d7449512e7a97ed678e4d181f7497b19347312c8
Andre-PI <andrelucas_PI@proton.me>
2025-01-17 10:33:47 -0300

    added a test page for the Connection component

--- ProjetoEvento/src/App.jsx
+++ ProjetoEvento/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import TestConnection from './pages/TestConnection';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='test' element={<TestConnection />}/>
       </Routes>
     </div>
   );
