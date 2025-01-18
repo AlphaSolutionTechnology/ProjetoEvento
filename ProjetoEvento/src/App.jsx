a1fd79c6cdc4075a8ba000de6599110530ef152c
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-01-18 12:02:30 -0300

    nome logo

--- ProjetoEvento/src/App.jsx
+++ ProjetoEvento/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       {/* Header com o botão de alternância de tema */}
       <header className="p-4 flex justify-between items-center bg-gray-200 dark:bg-gray-800">
-        <h1 className="text-xl font-bold">Minha Aplicação</h1>
+        <h1 className="text-xl font-bold">LOGO</h1>
         <ThemeToggle />
       </header>
 
