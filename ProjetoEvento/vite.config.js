5d322afc7ac0ceb9adae026bedc41e23fbad275e
pvdias0 <p.vitordias@outlook.com>
2025-01-21 00:36:18 -0300

    correcao arquivo vite.config.js

--- ProjetoEvento/vite.config.js
+++ ProjetoEvento/vite.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Permite conexões externas
     port: 5173,      // Porta padrão do Vite
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '192.168.18.94-key.pem')), // Caminho da chave privada
-      cert: fs.readFileSync(path.resolve(__dirname, '192.168.18.94.pem')),    // Caminho do certificado
-    },
+    https:false
   },
 });
