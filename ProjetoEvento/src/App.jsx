2e7f6eb1588b9318875dec7dbabbaabdfba483fc
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-01-23 19:05:50 -0300

    fix import

--- ProjetoEvento/src/App.jsx
+++ ProjetoEvento/src/App.jsx
@@ -7,10 +7,11 @@ import TestConnection from "./pages/TestConnection";
 import Home from "./pages/home";
 import LoginPage from "./pages/LoginPage";
 import { Test } from "./pages/Test";
-import CreateQuestoes from "./pages/createquestoes";
+import CreateQuestoes from "./components/createquestoes";
 import ParentComponent from "./pages/ParentComponent";
 import WebSTest from "./pages/WebSTest";
 import { WebSocketProvider } from "./context/WebSocketContext"; // Importa o contexto WebSocket
+import PalestrasList from "./pages/PalestrasList";
 
 function App() {
   const { user } = useAuth(); // pode ser null ou com dados
@@ -35,7 +36,7 @@ function App() {
           <Route path='/palestras' element={<PalestrasList/>}/>
           <Route path="/parent" element={<ParentComponent />} />
           <Route path="/websocket" element={<WebSTest />} />
-        <Route path='/admQuizz' element={<AdmQuizz/>}/>
+        <Route path='/admQuizz' element={<CreateQuestoes/>}/>
         </Routes>
       </div>
     </WebSocketProvider>
