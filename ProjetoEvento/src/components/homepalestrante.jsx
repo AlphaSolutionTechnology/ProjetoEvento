4e695b1c630be7c344f90e02b3817d072efa18e0
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2025-01-24 16:47:49 -0300

    edit

--- ProjetoEvento/src/components/homepalestrante.jsx
+++ ProjetoEvento/src/components/homepalestrante.jsx
@@ -1,9 +1,15 @@
-
 // Componente HomePalestrante
+import React from 'react';
+import PalestrasList from '../pages/palestrasList';
+
 const HomePalestrante = () => {
     return (
         <div>
-            {/* Conteúdo do HomePalestrante */}
+            <h1 className="text-center text-2xl font-bold my-4">Bem-vindo ao Painel do Palestrante</h1>
+            <div className="palestras-section">
+                <h2 className="text-xl font-semibold my-2">Minhas Palestras</h2>
+                <PalestrasList />
+            </div>
         </div>
     );
 };
