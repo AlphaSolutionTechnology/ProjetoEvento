cbae15b726cd1305000bd0d56ec9657e38907ce0
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2025-01-24 16:02:37 -0300

    correção

--- ProjetoEvento/src/pages/home.jsx
+++ ProjetoEvento/src/pages/home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useTheme } from '../context/ThemeContext'; // Importa o hook de tema
 import { useAuth } from '../context/AuthContext';
-import HomeUser from './HomeUser'; // Importa o componente HomeUser
-import HomePalestrante from './HomePalestrante'; // Importa o componente HomePalestrante
+import HomeUser from '../components/HomeUser'; // Importa o componente HomeUser
+import HomePalestrante from '../components/HomePalestrante'; // Importa o componente HomePalestrante
 
 function Home() {
   return (
