c96af155afb86b490107f7d0448fa5763e2d51c1
Andre-PI <andrelucas_PI@proton.me>
2025-02-09 20:51:51 -0300

    adicionada a funcionalidade para aceitar ou recusar conexões(novamente)

--- ProjetoEvento/src/components/Ranking_Component/Ranking.jsx
+++ ProjetoEvento/src/components/Ranking_Component/Ranking.jsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./Ranking_CSS.css";
 
-const initialRankingData = [
-  { id: 1, name: "Tonho", points: 95 },
-  { id: 2, name: "Carlao", points: 169 },
-  { id: 3, name: "Andre", points: 120 },
-  { id: 4, name: "Bombado do Primavera", points: 105 },
-  {id: 5 , name: "Duardo", points: 104},
-  {id: 6, name:"Zé do Picadinho", points:66},
-];
-
 const Ranking = () => {
   const [rankingData, setRankingData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = [...initialRankingData];
-      const sortedData = data.sort((a, b) => b.points - a.points);
-      setRankingData(sortedData);
+      try {
+        const response = await fetch("http://localhost:8080/api/ranking/getupdatedranking");
+
+        if (!response.ok) {
+          throw new Error(`Erro na requisição: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setRankingData(data); 
+        console.log(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
@@ -26,24 +31,34 @@ const Ranking = () => {
   return (
     <div className="container">
       <h2>Ranking</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Posição</th>
-            <th>Nome</th>
-            <th>Pontos</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rankingData.map((user, index) => (
-            <tr key={user.id}>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.points}</td>
+
+      {loading && <p>Carregando...</p>}
+      {error && <p style={{ color: "red" }}>Erro: {error}</p>}
+
+      {!loading && !error && (
+        <table>
+          <thead>
+            <tr>
+              <th>Posição</th>
+              <th>Nome</th>
+              <th>Conexões</th> {/* Nova coluna */}
+              <th>Pontos do Quizz</th> {/* Nova coluna */}
+              <th>Pontuação Total</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {rankingData.map((user, index) => (
+              <tr key={user.id}>
+                <td>{user.colocacao}</td>
+                <td>{user.nomeUsuario}</td> 
+                <td>{user.conexoes}</td> 
+                <td>{user.acertos}</td> 
+                <td>{user.pontuacaoTotal}</td> 
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
