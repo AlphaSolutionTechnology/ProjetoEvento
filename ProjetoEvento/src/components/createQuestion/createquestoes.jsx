726e91764bcfa82eba5ddcb1dd8ae6f6d6a5e4db
Antonio Gleyser <antoniogleyser2@hotmail.com>
2025-02-17 11:09:31 -0300

    substituicao feedbackmessage por alert

--- ProjetoEvento/src/components/createQuestion/createquestoes.jsx
+++ ProjetoEvento/src/components/createQuestion/createquestoes.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useState, useCallback } from "react";
 import { useLocation } from "react-router-dom";
-import FeedbackMessage from "./FeedbackMessage";
+import AlertToast from "../alert/AlertToast.jsx";
 import QuestionsInvite from "./QuestionsInvite.jsx";
 import ChatComponent from "../AI/Groq.jsx"; // Importa o componente de geração de questões
+import AlertToast from "../alert/AlertToast.jsx";
 
 function CreateQuestoes() {
   const [questions, setQuestions] = useState([]);
   const [message, setMessage] = useState("");
+  const [alertType, setAlertType] = useState("warning");
+
   const location = useLocation();
   const idPalestra = location.state?.idPalestra;
 
   useEffect(() => {
     if (!idPalestra) {
       setMessage("ID da palestra não encontrado. Verifique o fluxo de navegação.");
+      setAlertType("error");
+      setOpenAlert(true);
     }
   }, [idPalestra]);
 
@@ -27,6 +32,9 @@ function CreateQuestoes() {
     };
 
     setQuestions((prevQuestions) => [...prevQuestions, formattedQuestion]);
+    setMessage("Questão gerada com sucesso.");
+    setAlertType("success");
+    setOpenAlert(true);
   }, []);
 
   return (
@@ -39,7 +47,14 @@ function CreateQuestoes() {
         setMessage={setMessage}
       />
 
-      <FeedbackMessage message={message} />
+      {/* Componente de alerta atualizado*/}
+      <AlertToast
+        open={message}
+        message={message}
+        type={alertType}
+        onClose={() => setOpenAlert(false)}
+      />
+
     </div>
   );
 }
